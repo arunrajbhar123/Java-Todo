ae3814f47e338d1cf7754f6c1c09135edb1b1688
Arun Rajbhar <arunrajbhar@mayhem-studios.com>
2024-04-26 11:47:50 +0530

    added delete fun

--- src/Methods.java
+++ src/Methods.java
@@ -3,8 +3,8 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Methods {
-private ArrayList<Task> taskArrayList;
-private Scanner scanner;
+private final ArrayList<Task> taskArrayList;
+private final Scanner scanner;
 
     public Methods() {
     taskArrayList = new ArrayList<>();
@@ -20,9 +20,20 @@ private Scanner scanner;
             taskArrayList.get(index).setCompleted();
 
         }else{
-            System.out.println("Please Enter Valid index or Id");
+            System.out.println("Please Enter Valid Id");
         }
     }
+
+    public void deleteTask(int index){
+        if(index>=0 && index < taskArrayList.size()){
+            taskArrayList.remove(index);
+
+        }else{
+            System.out.println("Please Enter Valid Id");
+        }
+
+    }
+
     public void displayTask(){
         int size=taskArrayList.size();
         System.out.println("Task :");
@@ -38,7 +49,8 @@ private Scanner scanner;
            System.out.println("1. Add Task.");
            System.out.println("2. Mark as completed.");
            System.out.println("3. Display the tasks.");
-           System.out.println("4. Exist.");
+           System.out.println("4. Delete Task.");
+           System.out.println("5. Exist.");
            System.out.println("Choose option:" );
            int option=scanner.nextInt();
            scanner.nextLine();
@@ -55,14 +67,20 @@ private Scanner scanner;
                    break;
                case 2:
                    System.out.println("Enter Id To Set IsCompleted:");
-                   int index=scanner.nextInt() -1;
+                   int index=scanner.nextInt() - 1;
                    setCompletedTask(index);
                    break;
                case 3:
                    displayTask();
                    break;
                case 4:
+                   System.out.println("Enter Id To Delete:");
+                   int i=scanner.nextInt()-1;
+                   deleteTask(i);
+                   break;
+               case 5:
                    System.out.println("Existing...");
+                   return;
                default:
                    System.out.println("Please enter valid input.");
                    break;
